41f354ff843cac0f2c12e30704e36f4b3ea936fc
Albertluiiz <luisguillen290301@gmail.com>
2023-07-22 23:57:36 -0500

    Plantilla

--- app/Http/Controllers/VistaController.php
+++ app/Http/Controllers/VistaController.php
@@ -19,4 +19,182 @@ class VistaController extends Controller
     {
         return view('auth-signin-basic');
     }
+    public function advanceUiAnimation()
+    {
+        return view('advance-ui-animation');
+    }
+    public function advanceUiHighlight()
+    {
+        return view('advance-ui-highlight');
+    }
+
+    public function advanceUiNestable()
+    {
+        return view('advance-ui-nestable');
+    }
+
+    public function advanceUiRatings()
+    {
+        return view('advance-ui-ratings');
+    }
+
+    public function advanceUiScrollbar()
+    {
+        return view('advance-ui-scrollbar');
+    }
+
+    public function advanceUiScrollspy()
+    {
+        return view('advance-ui-scrollspy');
+    }
+
+    public function advanceUiSweetalerts()
+    {
+        return view('advance-ui-sweetalerts');
+    }
+
+    public function advanceUiSwiper()
+    {
+        return view('advance-ui-swiper');
+    }
+
+    public function advanceUiTour()
+    {
+        return view('advance-ui-tour');
+    }
+
+    public function appsApiKey()
+    {
+        return view('apps-api-key');
+    }
+
+    public function appsCalendar()
+    {
+        return view('apps-calendar');
+    }
+
+    public function appsChat()
+    {
+        return view('apps-chat');
+    }
+
+    public function appsCrmCompanies()
+    {
+        return view('apps-crm-companies');
+    }
+
+    public function appsCrmContacts()
+    {
+        return view('apps-crm-contacts');
+    }
+
+    public function appsCrmDeals()
+    {
+        return view('apps-crm-deals');
+    }
+
+    public function appsCrmLeads()
+    {
+        return view('apps-crm-leads');
+    }
+
+    public function appsCryptoBuySell()
+    {
+        return view('apps-crypto-buy-sell');
+    }
+
+    public function appsCryptoIco()
+    {
+        return view('apps-crypto-ico');
+    }
+
+    public function appsCryptoKyc()
+    {
+        return view('apps-crypto-kyc');
+    }
+
+    public function appsCryptoOrders()
+    {
+        return view('apps-crypto-orders');
+    }
+
+    public function appsCryptoTransactions()
+    {
+        return view('apps-crypto-transactions');
+    }
+
+    public function appsCryptoWallet()
+    {
+        return view('apps-crypto-wallet');
+    }
+
+    public function appsEcommerceAddProduct()
+    {
+        return view('apps-ecommerce-add-product');
+    }
+
+    public function appsEcommerceCart()
+    {
+        return view('apps-ecommerce-cart');
+    }
+
+    public function appsEcommerceCheckout()
+    {
+        return view('apps-ecommerce-checkout');
+    }
+
+    public function appsEcommerceCustomers()
+    {
+        return view('apps-ecommerce-customers');
+    }
+
+    public function appsEcommerceOrderDetails()
+    {
+        return view('apps-ecommerce-order-details');
+    }
+
+    public function appsEcommerceOrders()
+    {
+        return view('apps-ecommerce-orders');
+    }
+
+    public function appsEcommerceProductDetails()
+    {
+        return view('apps-ecommerce-product-details');
+    }
+
+    public function appsEcommerceProducts()
+    {
+        return view('apps-ecommerce-products');
+    }
+
+    public function appsEcommerceSellerDetails()
+    {
+        return view('apps-ecommerce-seller-details');
+    }
+
+    public function appsEcommerceSellers()
+    {
+        return view('apps-ecommerce-sellers');
+    }
+
+    public function appsEmailBasic()
+    {
+        return view('apps-email-basic');
+    }
+
+    public function appsEmailEcommerce()
+    {
+        return view('apps-email-ecommerce');
+    }
+
+    public function appsFileManager()
+    {
+        return view('apps-file-manager');
+    }
+
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,52 @@ Route::get('/', jobController::class);  // Controla una unica vista
 Route::get('home', [VistaController::class, 'index']);
 Route::get('home/job-landing', [VistaController::class, 'jobLanding']);
 Route::get('home/auth-signin-basic', [VistaController::class, 'authSigninBasic']);
+Route::get('home/advance-ui-animation', [VistaController::class, 'advanceUiAnimation']);
+Route::get('home/advance-ui-highlight', [VistaController::class, 'advanceUiHighlight']);
+Route::get('home/advance-ui-nestable', [VistaController::class, 'advanceUiNestable']);
+Route::get('home/advance-ui-ratings', [VistaController::class, 'advanceUiRatings']);
+Route::get('home/advance-ui-scrollbar', [VistaController::class, 'advanceUiScrollbar']);
+Route::get('home/advance-ui-scrollspy', [VistaController::class, 'advanceUiScrollspy']);
+Route::get('home/advance-ui-sweetalerts', [VistaController::class, 'advanceUiSweetalerts']);
+Route::get('home/advance-ui-swiper', [VistaController::class, 'advanceUiSwiper']);
+Route::get('home/advance-ui-tour', [VistaController::class, 'advanceUiTour']);
+Route::get('home/apps-api-key', [VistaController::class, 'appsApiKey']);
+Route::get('home/apps-calendar', [VistaController::class, 'appsCalendar']);
+Route::get('home/apps-chat', [VistaController::class, 'appsChat']);
+Route::get('home/apps-crm-companies', [VistaController::class, 'appsCrmCompanies']);
+
+
+
+
+
+
+Route::get('home/apps-crm-contacts', [VistaController::class, 'appsCrmContacts']);
+Route::get('home/apps-crm-deals', [VistaController::class, 'appsCrmDeals']);
+Route::get('home/apps-crm-leads', [VistaController::class, 'appsCrmLeads']);
+Route::get('home/apps-crypto-buy-sell', [VistaController::class, 'appsCryptoBuySell']);
+Route::get('home/apps-crypto-ico', [VistaController::class, 'appsCryptoIco']);
+Route::get('home/apps-crypto-kyc', [VistaController::class, 'appsCryptoKyc']);
+Route::get('home/apps-crypto-orders', [VistaController::class, 'appsCryptoOrders']);
+Route::get('home/apps-crypto-transactions', [VistaController::class, 'appsCryptoTransactions']);
+Route::get('home/apps-crypto-wallet', [VistaController::class, 'appsCryptoWallet']);
+Route::get('home/apps-ecommerce-add-product', [VistaController::class, 'appsEcommerceAddProduct']);
+Route::get('home/apps-ecommerce-cart', [VistaController::class, 'appsEcommerceCart']);
+Route::get('home/apps-ecommerce-checkout', [VistaController::class, 'appsEcommerceCheckout']);
+Route::get('home/apps-ecommerce-customers', [VistaController::class, 'appsEcommerceCustomers']);
+Route::get('home/apps-ecommerce-order-details', [VistaController::class, 'appsEcommerceOrderDetails']);
+Route::get('home/apps-ecommerce-orders', [VistaController::class, 'appsEcommerceOrders']);
+Route::get('home/apps-ecommerce-product-details', [VistaController::class, 'appsEcommerceProductDetails']);
+Route::get('home/apps-ecommerce-products', [VistaController::class, 'appsEcommerceProducts']);
+Route::get('home/apps-ecommerce-seller-details', [VistaController::class, 'appsEcommerceSellerDetails']);
+Route::get('home/apps-ecommerce-sellers', [VistaController::class, 'appsEcommerceSellers']);
+Route::get('home/apps-email-basic', [VistaController::class, 'appsEmailBasic']);
+Route::get('home/apps-email-ecommerce', [VistaController::class, 'appsEmailEcommerce']);
+Route::get('home/apps-file-manager', [VistaController::class, 'appsFileManager']);
+
+
+
+
+
+
+
+
